8b1d31bd3f3a98fc7939b4ccc1b9b4fa36282639
xuyu <xypisces@163.com>
2018-12-07 17:48:06 +0800

    test

--- test/interview.js
+++ test/interview.js
@@ -1,9 +1,27 @@
-function foo(){
-  let a = 20;
-  let b = 310;
-  return function bar(){
-    return a + b
-  }
+// function foo(){
+//   let a = 20;
+//   let b = 310;
+//   return function bar(){
+//     return a + b
+//   }
+// }
+// var bar = foo()
+// console.log(bar())
+
+Array.prototype.multiply = function(){
+  this.forEach(i => {
+    this.push(i*i)
+  })
+  return this
 }
-var bar = foo()
-console.log(bar())
+
+const a = [1,2,3,4,5]
+a.multiply()
+console.log(a);
+
+// const a = {
+//   key1: Symbol(),
+//   key2: 10
+// }
+
+// console.log(JSON.stringify(a))
