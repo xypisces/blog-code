a534fffe540a7f1acc85f5f1571995629fc5769d
xuyu <xypisces@163.com>
2019-01-02 18:32:57 +0800

    webpack-update

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,35 @@
 const path = require("path")
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   mode: "production",
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
   },
   output: {
     publicPath: __dirname + "/dist/",
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js"
+    filename: "[name].[contenthash].js"
+  },
+  plugins:[
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'cache webpack'
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups:{
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        }
+      }
+    }
   },
   module:{
     rules:[{
